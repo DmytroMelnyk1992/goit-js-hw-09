a418f7104b24d0504b554c17ebfac0298d026eef
Dmytro Melnyk <dmytro.melnyk.1992@gmail.com>
2022-09-04 21:19:52 +0300

    debugging

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,28 +1,24 @@
+const body = document.querySelector('body');
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const startBtn = document.querySelector('[data-start]');
-const stopBtn = document.querySelector('[data-stop]');
-const bodyColorSwitch = document.querySelector('body');
-
-stopBtn.disabled = true;
-
-startBtn.onclick = () => {
-  console.log(`start switch`);
-
-  startBtn.disabled = true;
-  stopBtn.disabled = false;
-
+stop.disabled = true;
+start.onclick = () => {
+  console.log(`start`);
+  start.disabled = true;
+  stop.disabled = false;
   timer = setInterval(intervalColor, 1000);
   intervalColor();
 };
-
 const intervalColor = () => {
-  bodyColorSwitch.style.cssText = `background-color: ${getRandomHexColor()};`;
-  stopBtn.onclick = () => {
-    console.log(`stop switch`);
-    startBtn.disabled = false;
-    stopBtn.disabled = true;
+  body.style.cssText = `background-color: ${getRandomHexColor()};`;
+  stop.onclick = () => {
+    console.log(`stop`);
+    start.disabled = false;
+    stop.disabled = true;
     clearInterval(timer);
   };
 };
