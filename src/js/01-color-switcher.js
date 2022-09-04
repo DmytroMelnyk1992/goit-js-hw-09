098e336b5c8e84a3a46e3e9bdadff9b91d60fa63
Dmytro Melnyk <dmytro.melnyk.1992@gmail.com>
2022-09-04 19:36:47 +0300

    switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyColorSwitch = document.body;
 
+stopBtn.disabled = true;
+
+startBtn.onclick = () => {
+  console.log(`start switch`);
+
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  timer = setInterval(intervalColor, 1000);
+  intervalColor();
+};
+
+const intervalColor = () => {
+  bodyColorSwitch.style.cssText = `background-color: ${getRandomHexColor()};`;
+  stopBtn.onclick = () => {
+    console.log(`stop switch`);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(timer);
+  };
+};
